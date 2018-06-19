350f00148baebf29081b2c1933804e37959dd1d5
Screemy <info@jirickajakub.cz>
2018-06-19 21:51:42 +0200

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# Script, jehož funkcí je přidat soubory s překladem do českého jazyka v Zimbra Collaboration Server Open Source Edition (8.8.8GA) #
+# Můžeme také nastavit důležité proměnné v konfiguraci serveru Zimbra (související se zobrazením implementovaného jazyka) #
+
+echo ""
+echo " ##################################################################################"
+echo " ###																			  ###"
+echo " ###			-- Cesky preklad pro Zimbru --			                          ###"
+echo " ###										                                      ###"
+echo " ###	Zimbra Collaboration Server Open Source Edition (8.8.8GA)			      ###"
+echo " ###																			  ###"
+echo " ###		Autor: Jiricka Jakub		Date: 19-06-2018						  ###"
+echo " ###		Kontakt: info@jirickajakub.cz										  ###"
+echo " ###																			  ###"
+echo " ##################################################################################"
+echo ""
+
+num1=0
+while [ $num1 == 0 ]
+do
+	read -p "· Chcete implementovat cesky preklad na Zimbru? [A / N]:" INSTALACE;
+	if [ "$INSTALACE" == "A" ] || [ "$INSTALACE" == "a" ]; then
+		# Zmena prav a uzivatle k jazykovym souborum.
+		/bin/chmod 664 messages/*
+		/bin/chmod 664 keys/*
+		/bin/chmod 664 msgs/*
+		/bin/chown zimbra:zimbra messages/*
+		/bin/chown zimbra:zimbra keys/*
+		/bin/chown zimbra:zimbra msgs/*
+
+		# Zkopírujeme jazykové soubory na jejich odpovídající místa.
+		cp -fp messages/* /opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/messages/
+		cp -fp keys/* /opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/keys/
+		cp -fp msgs/* /opt/zimbra/conf/msgs/
+		cp -fp messages/* /opt/zimbra/jetty/webapps/zimbraAdmin/WEB-INF/classes/messages/
+		cp -fp keys/* /opt/zimbra/jetty/webapps/zimbraAdmin/WEB-INF/classes/keys/
+
+		# Přidejte localeName_cz = Czech do souborů ZmMsg_XX.properties jednotlivých jazyků.
+		for file1 in /opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/messages/ZmMsg_*; 
+			do
+				echo "localeName_cz = Czech" >> $file1;
+			done
+
+		for file2 in /opt/zimbra/jetty/webapps/zimbraAdmin/WEB-INF/classes/messages/ZmMsg_*;
+			do
+				echo "localeName_cz = Czech" >> $file2;
+			done
+
+		# Zkopírujeme soubory nápovědy.
+		su - zimbra -c "cp -fpr /opt/zimbra/jetty/webapps/zimbra/help/en_US/ /opt/zimbra/jetty/webapps/zimbra/help/eu"
+		su - zimbra -c "cp -fpr /opt/zimbra/jetty/webapps/zimbraAdmin/help/en_US/ /opt/zimbra/jetty/webapps/zimbraAdmin/help/eu"
+
+				echo "";
+				echo " Jazyk byl spravne implementovan";
+				echo " Poznámka: V některých částech Zimbry, které jsou závislé na Zimlets, nemusí být uvedeným jazykem cestina. Preklad zimletu nespada do rozsahu tohoto projektu.";
+				echo "";
+				num1=1;
+	fi
+	if [ "$INSTALACE" == "N" ] || [ "$INSTALACE" == "n" ]; then
+		echo "";
+		echo " Instalace byla zrusena!";
+		echo "";
+		num1=1;
+	exit;
+	fi
+done
+
+num2=0
+while [ $num2 == 0 ]
+do
+	read -p "· Chcete nastavit cestinu jako výchozí jazyk pro všechny uživatele v rozhraní Zimbra? (Pokud používají výchozí CoS) [A / N]: " DEFAULT;
+	if [ "$DEFAULT" == "A" ] || [ "$DEFAULT" == "a" ]; then
+		# ZimbraPrefLocale jsme nastavili do cestiny
+		su - zimbra -c "zmprov mc default zimbraPrefLocale cz"
+		echo "";
+		echo " Nastavena cestina jako vychozi jazyk ve webovem rozhrani Zimbry";
+		echo " Poznámka: Pokud uživatel provede vlastní konfiguraci jazyka z jejich předvoleb, bude jejich volba nad výchozí konfigurací serveru.";
+		echo " Poznámka: Pokud je CoS vlastní / specifické pro vaše skupiny uživatelů, musíte je ručně změnit příkazem: 'zmprov mc NONMBRE_DE_TU_CoS zimbraPrefLocale cz'" 
+		echo "";
+		num2=1;
+	fi
+	if [ "$DEFAULT" == "N" ] || [ "$DEFAULT" == "n" ]; then
+		echo "";
+        	echo " Krok byl vynechán.";
+		echo "";
+		num2=1;
+	fi
+done
+
+num3=0
+while [ $num3 == 0 ]
+do
+	read -p "· Chcete-li nový jazyk zpřístupnit, musíte restartovat službu Zimbra. Chcete jej nyní restartovat? [A / N]: " RESTART;
+	if [ "$RESTART" == "A" ] || [ "$RESTART" == "a" ]; then
+		echo ""
+		echo " Restartování služby Zimbra. Tento proces může trvat pár minut ...";
+		echo ""
+		# Restartujeme sluzby
+		su - zimbra -c "zmcontrol stop";
+		sleep 10;
+		echo "";
+		su - zimbra -c "zmcontrol startup";
+		sleep 10;
+		echo "";
+		echo " Služba Zimbra byla restartována. Instalace byla dokončena.";
+		echo "";
+		num3=1;
+	fi
+	if [ "$RESTART" == "N" ] || [ "$RESTART" == "n" ]; then
+		echo "";
+		echo " Rozhodli jste se vynechat službu RESTART služby Zimbra. Nezapomeňte, že nový jazyk nebude dostupný, dokud nebude služba Zimbra restartována.";
+		echo "";
+		num3=1;
+	fi
+done
+
